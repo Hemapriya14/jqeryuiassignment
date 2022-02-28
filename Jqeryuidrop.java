7dbd9615aded90e72b9e5f8ca592931a79bc2c9d
Hemapriya14 <99642541+Hemapriya14@users.noreply.github.com>
2022-02-28 12:22:34 +0530

    Add files via upload

---
+++ Jqeryuidrop.java
@@ -0,0 +1,33 @@
+package week3.day5aasign;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Jqeryuidrop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://jqueryui.com/droppable");
+		driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+			
+			//drop
+			driver.switchTo().frame(0);//frame  alwaysstarts index is 0
+			
+			WebElement source=driver.findElement(By.id("draggable"));
+				WebElement dest=driver.findElement(By.id("droppable"));
+				Actions builder=new Actions(driver);
+				builder.dragAndDrop(source,dest).build().perform();
+			driver.switchTo().defaultContent();
+
+	}
+
+}
